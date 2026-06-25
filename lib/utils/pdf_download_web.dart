7f5300122d5de78a07aeb5b0b8b9aab47c891351
Vince <vinceevangelista028@gmail.com>
2026-06-26 04:43:35 +0800

    fix: remove unused anchor variable warning in pdf download web helper

--- lib/utils/pdf_download_web.dart
+++ lib/utils/pdf_download_web.dart
@@ -3,7 +3,7 @@ import 'dart:html' as html;
 void downloadPdf(List<int> bytes, String filename) {
   final blob = html.Blob([bytes], 'application/pdf');
   final url = html.Url.createObjectUrlFromBlob(blob);
-  final anchor = html.AnchorElement(href: url)
+  html.AnchorElement(href: url)
     ..setAttribute("download", filename)
     ..click();
   html.Url.revokeObjectUrl(url);
